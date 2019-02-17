6782e40f6f0539942f790b45947b4e34666b8bdf
daiyinger <1002639334@qq.com>
2019-02-17 17:12:58 +0800

    busy box backup

--- gen_rootfs.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-sudo rm -rf output
-mkdir output 
-sudo cp -rf * output/
-cd output
-rm -rf .git*
-sudo rm -rf output
-find . -not -name "gen_rootfs.sh" -not -name "rootfs.cpio" | cpio -o -H newc > rootfs.cpio 
-gzip -c rootfs.cpio > rootfs.cpio.gz
--- home/config_cifs.sh
+++
@@ -1,2 +0,0 @@
-mkdir /mnt/cifs
-mount -t cifs -o username=xiaobo,password=199105jaCDU //192.168.3.7/code /mnt/cifs
